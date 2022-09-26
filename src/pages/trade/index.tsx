2e831eef24cb67dd031a3d3cc80bd87301cd03f2
Burt <1020103647@qq.com>
2022-09-26 20:53:42 +0800

    feat: Trade

---
+++ src/pages/trade/index.tsx
@@ -0,0 +1,11 @@
+import BaseLayout from '@/layouts/BaseLayout'
+
+const Trade = () => {
+  return (
+    <BaseLayout pageTitle='Trade' pageDesc='Perpetual trading'>
+      Trade
+    </BaseLayout>
+  )
+}
+
+export default Trade
