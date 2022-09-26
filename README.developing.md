85ec204c2833c058b0e2f675a5d965d39c47e3ec
Burt <1020103647@qq.com>
2022-09-26 13:29:14 +0800

    refactor: ConnectWallet

--- README.developing.md
+++ README.developing.md
@@ -8,7 +8,12 @@ pnpm create next-app --ts <project-name>
 ```
 ### import `Tailwindcss`
 <!-- https://tailwindcss.com/docs/installation -->
-```
+```sh
 pnpm add -D tailwindcss postcss autoprefixer cssnano
 npx tailwindcss init -p
+```
+
+### connect wallet
+```sh
+pnpm add -S ethers wagmi @rainbow-me/rainbowkit
 ```
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { WagmiConfig } from 'wagmi'
+import { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import { chains, wagmiClient } from '@/config/wagmi'
+import 'styles/globals.css'
+import 'styles/rainbowkit.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return null
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
 
 export default MyApp
