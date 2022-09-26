89eba673885603250cb2a9c4d69d8887889a7506
Burt <1020103647@qq.com>
2022-09-26 17:19:50 +0800

    fix: ConnectWallet Button error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['chainlist.org'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { AppProps } from 'next/app'
 import { useEffect, useState } from 'react'
 import { chains, wagmiClient } from '@/config/wagmi'
 import '@/styles/globals.css'
-import '@/styles/rainbowkit.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false)
